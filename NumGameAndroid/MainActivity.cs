a1bfea53bf335bb596d22b0b6760001c0c7b3752
NENightElves <nenightelves@hotmail.com>
2016-03-18 23:08:58 +0800

    添加安卓移植

--- NumGameAndroid/MainActivity.cs
+++ NumGameAndroid/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using num_game_core;
 
 namespace NumGameAndroid
 {
@@ -12,6 +13,12 @@ namespace NumGameAndroid
     public class MainActivity : Activity
     {
         int count = 1;
+
+        int target;
+        int[] c = new int[3];
+        int[] p = new int[3];
+        int step_tmp;
+
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -23,9 +30,215 @@ namespace NumGameAndroid
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
-
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;
+            button4.Click += button4_Click;
+            button5.Click += button5_Click;
+            button6.Click += button6_Click;
+
+            reset();
+
         }
+
+        #region 必要函数
+        private void check_target()
+        {
+
+        }
+
+        private void reset()
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+
+            button1.Text = Convert.ToString(1);
+            button2.Text = Convert.ToString(1);
+            button3.Text = Convert.ToString(1);
+            button4.Text = Convert.ToString(1);
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Text = "开始，电脑先";
+            button6.Text = "开始，玩家先";
+            button5.Enabled = true;
+            button6.Enabled = true;
+        }
+
+        private bool check_win()
+        {
+            TextView editText1 = FindViewById<TextView>(Resource.Id.editText1);
+
+            if ((c[1] == 9) && (c[2] == 9)) { editText1.Text = "电脑胜利！"; reset(); return true; }
+            if ((p[1] == 9) && (p[2] == 9)) { editText1.Text = "玩家胜利！"; reset(); return true; }
+            return false;
+        }
+
+        private void make_text()
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+
+            button1.Text = Convert.ToString(c[1]);
+            button2.Text = Convert.ToString(c[2]);
+            button3.Text = Convert.ToString(p[1]);
+            button4.Text = Convert.ToString(p[2]);
+        }
+
+        private void plus(int ri, int rj)
+        {
+            c[ri] = (c[ri] + p[rj]) % 10;
+            make_text();
+
+            check_win();
+        }
+
+        private void plus_p(int i, int j)
+        {
+            p[i] = (p[i] + c[j]) % 10;
+            make_text();
+        }
+        #endregion
+
+
+        #region button事件
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+            TextView editText1 = FindViewById<TextView>(Resource.Id.editText1);
+
+            if (button6.Enabled == false) { reset(); return; }
+            c[1] = 1; c[2] = 1; p[1] = 1; p[2] = 1;
+            make_text();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button6.Enabled = false;
+            button5.Text = "重置";
+            check_target();
+            target = Convert.ToInt32(editText1.Text);
+
+            numgamecomputing num = new numgamecomputing(target);
+            num.choosecompleted += plus;
+            num.generate(c, p, 0);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+            TextView editText1 = FindViewById<TextView>(Resource.Id.editText1);
+
+            c[1] = 1; c[2] = 1; p[1] = 1; p[2] = 1;
+            make_text();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button6.Enabled = false;
+            button5.Text = "重置";
+            check_target();
+            target = Convert.ToInt32(editText1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+
+            button3.Enabled = false; button4.Enabled = false;
+            button1.Enabled = true; button2.Enabled = true;
+            step_tmp = 1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+
+            button3.Enabled = false; button4.Enabled = false;
+            button1.Enabled = true; button2.Enabled = true;
+            step_tmp = 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+
+            plus_p(step_tmp, 1);
+
+            button3.Enabled = true; button4.Enabled = true;
+            button1.Enabled = false; button2.Enabled = false;
+
+            if (check_win()) return;
+
+            numgamecomputing num = new numgamecomputing(target);
+            num.choosecompleted += plus;
+            num.generate(c, p, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+
+            plus_p(step_tmp, 2);
+
+            button3.Enabled = true; button4.Enabled = true;
+            button1.Enabled = false; button2.Enabled = false;
+
+            if (check_win()) return;
+
+            numgamecomputing num = new numgamecomputing(target);
+            num.choosecompleted += plus;
+            num.generate(c, p, 0);
+        }
+        #endregion
+
     }
 }
 
---
+++ NumGameAndroid/numgamecomputing.cs
@@ -0,0 +1,245 @@
+﻿//核心代码版本v0.1.006Alpha
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace num_game_core
+{
+    class numgamecomputing
+    {
+
+        private int m, n;
+        private int[,] sp = new int[3, 3];
+        private int endx;
+        private int ri, rj;
+        private int target;
+        public delegate void choosecompleted_event(int a, int b);
+        public event choosecompleted_event choosecompleted;
+
+        public numgamecomputing(int a)
+        //a为目标数字
+        {
+            int i, j;
+            for (i = 1; i <= 2; i++)
+                for (j = 1; j <= 2; j++)
+                    sp[i, j] = 0;
+            m = 0; n = 0;
+            endx = 1;
+            //endx最好是奇数，并且大于0。在目前的generate下，过大可能导致抉择错误。
+            target = a;
+        }
+
+
+        #region 事件性质的函数
+        public void generate(int[] c, int[] p, int x)
+        //x请用0带入！
+        //c为电脑当前数字
+        //p为玩家当前数字
+        {
+            int i, j, tmp;
+            int[] tc = new int[3];
+            int[] tp = new int[3];
+            if (x > endx) return;
+
+            //初始校正
+            if (x == 0)
+            {
+                if (((c[1] + p[1]) % 10) == target) sp[1, 1] += 8;
+                if (((c[1] + p[2]) % 10) == target) sp[1, 2] += 8;
+                if (((c[2] + p[1]) % 10) == target) sp[2, 1] += 8;
+                if (((c[2] + p[2]) % 10) == target) sp[2, 2] += 8;
+
+                if ((((c[1] + p[1]) % 10) == target) && (p[2] == target)) { sp[1, 1] = 1000;sp[1, 2] = 1000; }
+                if ((((c[1] + p[2]) % 10) == target) && (p[1] == target)) { sp[1, 1] = 1000; sp[1, 2] = 1000; }
+                if ((((c[2] + p[1]) % 10) == target) && (p[2] == target)) { sp[2, 1] = 1000; sp[2, 2] = 1000; }
+                if ((((c[2] + p[2]) % 10) == target) && (p[1] == target)) { sp[2, 1] = 1000; sp[2, 2] = 1000; }
+            }
+
+            for (i = 1; i <= 2; i++)
+                for (j = 1; j <= 2; j++)
+                {
+
+                    tc[1] = c[1]; tc[2] = c[2];
+                    tp[1] = p[1]; tp[2] = p[2];
+
+                    if (x == 0) { m = i; n = j; }
+
+                    if ((x % 2) == 0)
+                    {
+                        //电脑的局
+                        tmp = (c[i] + p[j]) % 10;
+                        tc[i] = tmp;
+                        if (tc[i] == target) sp[m, n] += 3;
+                        //TEMP:else if (tc[i] != tc[3 - i]) sp[m, n]++;
+                        //电脑双target自动校正
+                        if ((x == 0) && (tc[1] == target) && (tc[2] == target))
+                        { sp[m, n] = int.MaxValue; choose(c, p); return; }
+                        //电脑双target自动校正
+                        generate(tc, tp, x + 1);
+                    }
+                    else
+                    {
+                        //玩家的局
+                        tmp = (p[i] + c[j]) % 10;
+                        tp[i] = tmp;
+                        if (tp[i] == target) sp[m, n] -= 3;
+                        //TEMP:else if (tp[i] != tp[3 - i]) sp[m, n]--;
+                        //玩家双target自动校正
+                        if ((x == 1) && (tp[1] == target) && (tp[2] == target) && (sp[m, n] != int.MaxValue))
+                        { sp[m, n] = int.MinValue; return; }
+                        //玩家双target自动校正                        
+                        generate(tc, tp, x + 1);
+                    }
+                }
+
+            if (x == 0)
+            {
+                //除9特例，防止拆9
+                for (i = 1; i <= 2; i++)
+                    for (j = 1; j <= 2; j++)
+                        if ((c[i] == target) && (sp[i, j] != int.MinValue)) sp[i, j] -= 8;
+                choose(c, p);
+            }
+        }
+
+
+
+
+        private void choose(int[] c, int[] p)
+        {
+            int i, j, max;
+            Random ran;
+            ran = new Random();
+            max = int.MinValue;
+
+            //TEST:优先级测试
+            Console.Write($"c[1]={c[1]}     c[2]={c[2]}     ");
+            Console.WriteLine($"p[1]={p[1]}     p[2]={p[2]}     ");
+            Console.Write($"sp[1,1]={sp[1, 1]}     sp[1,2]={sp[1, 2]}     ");
+            Console.WriteLine($"sp[2,1]={sp[2, 1]}     sp[2,2]={sp[2, 2]}     \r\n");
+            //
+
+            for (i = 1; i <= 2; i++)
+                for (j = 1; j <= 2; j++)
+                {
+                    if (max < sp[i, j]) { max = sp[i, j]; ri = i; rj = j; }
+                    else if (max == sp[i, j])
+                    {
+                        if ((ran.Next(0, 2) == 0) || (sp[i, j] == int.MinValue))
+                        { max = sp[i, j]; ri = i; rj = j; }
+                    }
+                }
+            choosecompleted(ri, rj);
+            //该事件为传回数据指用电脑的第ri个数加玩家的第rj个数之和，累加到电脑的第ri个数
+        }
+
+
+        #endregion
+        
+
+        #region 回传值性质的函数
+        public void generate(int[] c, int[] p, int x, ref int ri, ref int rj)
+        //x请用0带入！
+        //c为电脑当前数字
+        //p为玩家当前数字
+        //有回传值ri和rj
+        {
+            int i, j, tmp;
+            int[] tc = new int[3];
+            int[] tp = new int[3];
+            if (x > endx) return;
+
+            //初始校正
+            if (x == 0)
+            {
+                if (((c[1] + p[1]) % 10) == target) sp[1, 1] += 8;
+                if (((c[1] + p[2]) % 10) == target) sp[1, 2] += 8;
+                if (((c[2] + p[1]) % 10) == target) sp[2, 1] += 8;
+                if (((c[2] + p[2]) % 10) == target) sp[2, 2] += 8;
+
+                if ((((c[1] + p[1]) % 10) == target) && (p[2] == target)) { sp[1, 1] = 1000; sp[1, 2] = 1000; }
+                if ((((c[1] + p[2]) % 10) == target) && (p[1] == target)) { sp[1, 1] = 1000; sp[1, 2] = 1000; }
+                if ((((c[2] + p[1]) % 10) == target) && (p[2] == target)) { sp[2, 1] = 1000; sp[2, 2] = 1000; }
+                if ((((c[2] + p[2]) % 10) == target) && (p[1] == target)) { sp[2, 1] = 1000; sp[2, 2] = 1000; }
+            }
+
+            for (i = 1; i <= 2; i++)
+                for (j = 1; j <= 2; j++)
+                {
+
+                    tc[1] = c[1]; tc[2] = c[2];
+                    tp[1] = p[1]; tp[2] = p[2];
+
+                    if (x == 0) { m = i; n = j; }
+
+                    if ((x % 2) == 0)
+                    {
+                        //电脑的局
+                        tmp = (c[i] + p[j]) % 10;
+                        tc[i] = tmp;
+                        if (tc[i] == target) sp[m, n] += 3;
+                        //TEMP:else if (tc[i] != tc[3 - i]) sp[m, n]++;
+                        //电脑双target自动校正
+                        if ((x == 0) && (tc[1] == target) && (tc[2] == target))
+                        { sp[m, n] = int.MaxValue; choose(c, p, ref ri, ref rj); return; }
+                        //电脑双target自动校正
+                        generate(tc, tp, x + 1);
+                    }
+                    else
+                    {
+                        //玩家的局
+                        tmp = (p[i] + c[j]) % 10;
+                        tp[i] = tmp;
+                        if (tp[i] == target) sp[m, n] -= 3;
+                        //TEMP:else if (tp[i] != tp[3 - i]) sp[m, n]--;
+                        //玩家双target自动校正
+                        if ((x == 1) && (tp[1] == target) && (tp[2] == target) && (sp[m, n] != int.MaxValue))
+                        { sp[m, n] = int.MinValue; return; }
+                        //玩家双target自动校正                        
+                        generate(tc, tp, x + 1);
+                    }
+                }
+
+            if (x == 0)
+            {
+                //除9特例，防止拆9
+                for (i = 1; i <= 2; i++)
+                    for (j = 1; j <= 2; j++)
+                        if ((c[i] == target) && (sp[i, j] != int.MinValue)) sp[i, j] -= 8;
+                choose(c, p, ref ri, ref rj);
+            }
+        }
+
+
+
+        private void choose(int[] c, int[] p, ref int ri, ref int rj)
+        {
+            int i, j, max;
+            Random ran;
+            ran = new Random();
+            max = int.MinValue;
+
+            //TEST:优先级测试
+            Console.Write($"c[1]={c[1]}     c[2]={c[2]}     ");
+            Console.WriteLine($"p[1]={p[1]}     p[2]={p[2]}     ");
+            Console.Write($"sp[1,1]={sp[1, 1]}     sp[1,2]={sp[1, 2]}     ");
+            Console.WriteLine($"sp[2,1]={sp[2, 1]}     sp[2,2]={sp[2, 2]}     \r\n");
+            //
+
+            for (i = 1; i <= 2; i++)
+                for (j = 1; j <= 2; j++)
+                {
+                    if (max < sp[i, j]) { max = sp[i, j]; ri = i; rj = j; }
+                    else if (max == sp[i, j])
+                    {
+                        if ((ran.Next(0, 2) == 0) || (sp[i, j] == int.MinValue))
+                        { max = sp[i, j]; ri = i; rj = j; }
+                    }
+                }
+        }
+
+        #endregion
+
+    }
+}
